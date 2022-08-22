de785cbf0a772318b329e7c7df8ed14469acc77b
kuma-w <occhoi90@gmail.com>
2022-08-22 19:36:27 +0900

    tile 갯수가 0일경우 숨기는 옵션 추가

--- src/components/Tiles.tsx
+++ src/components/Tiles.tsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import { defaultTyles } from '../data/Tiles';
 import Tile from './Tile';
-import { Box } from '@mui/material';
+import { Box, ToggleButton } from '@mui/material';
+import { CheckBox } from '@mui/icons-material';
+
+interface TileOption {
+  hideZeroTile: boolean;
+}
 
 const Tiles = () => {
   const [myTiles, setMyTiles] = useState(defaultTyles);
+  const [option, setOption] = useState<TileOption>({ hideZeroTile: false });
+
+  const handleToggleShowZeroTile = () => {
+    setOption({ ...option, hideZeroTile: !option.hideZeroTile });
+  };
 
   const decreaseTile = (key: number) => {
     setMyTiles(
@@ -22,11 +32,17 @@ const Tiles = () => {
   };
 
   return (
-    <Box sx={{ display: 'flex', width: '100%', height: '100%', flexWrap: 'wrap' }}>
-      {myTiles.map((tile) => {
-        return <Tile tile={tile} decreaseTile={decreaseTile} increaseTile={increaseTile} />;
-      })}
-    </Box>
+    <>
+      <Box sx={{ display: 'flex', width: '100%', height: '100%', flexWrap: 'wrap' }}>
+        {myTiles.map((tile) => {
+          return option.hideZeroTile ? (
+            tile.count !== 0 && <Tile tile={tile} decreaseTile={decreaseTile} increaseTile={increaseTile} />
+          ) : (
+            <Tile tile={tile} decreaseTile={decreaseTile} increaseTile={increaseTile} />
+          );
+        })}
+      </Box>
+    </>
   );
 };
 
